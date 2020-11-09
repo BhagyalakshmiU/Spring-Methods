37f241e67869933a1003fa5c8993b17fdc8e0ce5
Bhagyalakshmi U <b.mattakkarau@dxc.com>
2020-11-10 00:32:32 +0530

    First Commit

--- src/main/java/com/demo/controllers/myDemoController.java
+++ src/main/java/com/demo/controllers/myDemoController.java
@@ -1,70 +1,43 @@
 package com.demo.controllers;
 
-import java.io.FileOutputStream;
-
-//import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 
-//import com.demo.model.Account;
-
+import java.util.Random;
 
 @Controller
 public class myDemoController 
 {
-
-/*	@RequestMapping(value="/createAccount")
-	public String createAccount(@Valid @ModelAttribute ("aNewAccount") Account account, BindingResult result)
+	private String[] quotes= {"To be or not to be - shakespeare","Spring in natures way of saying Let's Party",
+			"The time is alwyas right to do what is right"};
+	@RequestMapping(value="/getQuote")
+	public String  getRandomQuote(Model model)
 	{
-		if(result.hasErrors())
-		{
-			System.out.println("Form has Errors");
-			return "createAccount";
-		}
-		System.out.println(account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
-		return "createAccount";
+		int rand=new Random().nextInt(quotes.length);
+		String randomQuote=quotes[rand];
+		model.addAttribute("randomQuote",randomQuote);
+		return "quote";
 	}
-
-	@RequestMapping(value="/accountCreated",method=RequestMethod.POST)
-	public String performance(Account account)
+	@ModelAttribute
+	public void setUserDetails(@RequestParam("userName") String userName, Model model)
 	{
+		model.addAttribute("userName",userName);
+		String userRole="undefined";
+		if(userName.equals("Andy"))
+		{
+			userRole = "Student";
+		}
+		else if(userName.equals("Allana"))
+		{
+			userRole = "Dean";
+		}
+		model.addAttribute("userRole",userRole);
 		
-		System.out.println(account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
-		return "accountCreated";
-	}*/
-	@RequestMapping(value="/myForm")
-	public String myForm()
-	{
-		return "myForm";
+		System.out.println("Model updated with user information.");
 	}
-	@RequestMapping(value="/handleForm")
-	public String handleForm(@RequestParam("file") MultipartFile file)
-	{
-		try
-		{
-			if(!file.isEmpty())
-			{
-				byte[] bytes=file.getBytes();
-				FileOutputStream fos=new FileOutputStream("C:\\Users\\bmattakkarau\\OneDrive - DXC Production\\Pictures\\Saved Pictures\\myFile.jpg");
-				fos.write(bytes);
-				fos.close();
-				System.out.println("File saved successfully!!!");
-			}
-			else
-			{
-				System.out.println("No file available to save!");
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("Error saving file.");
-		}
-		return "operationComplete";
-	}
+	
+	
 }
